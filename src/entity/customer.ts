fcae77385b256880d1e090bbfc81911462025263
Wesley Prado <55308332+wesley-prado@users.noreply.github.com>
2024-01-23 11:07:53 -0300

    feat: create Customer entity class with basic functionality

---
+++ src/entity/customer.ts
@@ -0,0 +1,43 @@
+import Address from "./address";
+
+export default class Customer {
+	private id: string;
+	private name: string;
+	private address!: Address;
+	private active: boolean = false;
+
+	constructor(id: string, name: string) {
+		this.id = id;
+		this.name = name;
+
+		this.validate();
+	}
+
+	changeName(name: string): void {
+		this.name = name;
+
+		this.validate();
+	}
+
+	setAddress(address: Address): void {
+		this.address = address;
+	}
+
+	activate(): void {
+		if (this.address === undefined) {
+			throw new Error("Address is required to activate a customer");
+		}
+
+		this.active = true;
+	}
+
+	deactivate(): void {
+		this.active = false;
+	}
+
+	private validate() {
+		if (this.name.length < 3) {
+			throw new Error("Name must be at least 3 characters long");
+		}
+	}
+}
